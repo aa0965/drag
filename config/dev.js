f4d2ede00ae40b1a54a51fadfdbe37fa3368fb37
Anshu <aa0965@github.com>
2018-12-20 17:14:22 +0530

    'authdone'

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID :'22394680985-174qh2u3drcki3jlemeg9l6br1s9japq.apps.googleusercontent.com',
+  googleClientSecret: 'AgUvznKNNW8kHdzs_P4TQ_ga',
+
+  mongoURI : 'mongodb://anshups:anshups1@ds137340.mlab.com:37340/emaily-devv',
+  cookieKey: 'hbciebibeje'
+}
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+ require('./models/user.js');
+ require('./services/passport.js');
 
-app.get('/',(req,res) => {
-  res.send('HI');
-})
+ mongoose.connect(keys.mongoURI,{ useNewUrlParser: true });
+
+const app = express();
+require('./routes/authRoutes')(app);
+
+app.use(
+  cookieSession({
+    maxAge: 20*20*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
